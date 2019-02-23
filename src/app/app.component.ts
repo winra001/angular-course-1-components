37849eb9bb495d4db93c486d06adafb5f1b967f9
daniel ra <winra001@hotmail.com>
2019-02-24 08:32:38 +1100

    17. 18. 19. 20. View Child/Children Decorator and QueryList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
     course = COURSES[1];
 
@@ -16,7 +17,50 @@ export class AppComponent {
 
     startDate = new Date(2000, 0, 1);
 
+    @ViewChild(CourseCardComponent)
+    card: CourseCardComponent;
+
+    @ViewChild('cardRef1', {read: ElementRef})
+    card1: CourseCardComponent;
+
+    @ViewChild('container')
+    containerDiv: ElementRef;
+
+    @ViewChildren(CourseCardComponent)
+    cards: QueryList<CourseCardComponent>;
+
+    constructor() {
+        console.log('containerDiv - constructor()', this.containerDiv);
+    }
+
+    ngAfterViewInit() {
+        console.log('containerDiv - ngAfterViewInit()', this.containerDiv);
+
+        console.log('cards', this.cards);
+
+        this.cards.changes.subscribe(
+            cards => console.log('cards changed', cards)
+        );
+    }
+
     onCourseSelected(course: Course) {
         console.log('App component - clicked', course);
+        console.log('card', this.card);
+        console.log('card1', this.card1);
+        console.log('containerDiv', this.containerDiv);
     }
+
+    onCoursesEdited() {
+        this.courses.push(
+            {
+                id: 1,
+                description: "Angular Core Deep Dive",
+                iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+                longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+                category: 'INTERMEDIATE',
+                lessonsCount: 10
+            },
+        );
+    }
+
 }
