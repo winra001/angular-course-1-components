9c5131d24594ca3ece44aced2301fa6fcef1f8dd
daniel ra <winra001@hotmail.com>
2019-02-23 11:32:23 +1100

    16. Angular Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,11 @@ import { Course } from './model/course';
 })
 export class AppComponent {
 
+    course = COURSES[1];
+
     courses = COURSES;
+
+    startDate = new Date(2000, 0, 1);
 
     onCourseSelected(course: Course) {
         console.log('App component - clicked', course);
